0194e8f21a81ea20203850e0713dc6ca0f14749a
Raphael Matto <raphaelmatto@gmail.com>
2023-12-15 17:42:33 -0500

    Polish.

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ Scripts for Adobe InDesign.
 Open InDesign and choose Window -> Utilities -> Scripts, then right-click on the User folder
 and select "Reveal in Finder." Copy any script you want to use from this repo to
 that folder. After copying, it should show up in the User folder in the Scripts panel
-in InDesign. Right-click and choose "Run Script" to activate.
+in InDesign. Right-click and choose "Run Script" to activate, or double-click.
 
 ## Scripts
 
 ### RelinkBookDocuments
 
 Use this script to update paths for moved InDesign Documents in an InDesign Book.
-Tested on MacOs, probably doesn't work on Windows.
+Tested on MacOs, might work on Windows.
--- RelinkBookDocuments.jsx
+++ RelinkBookDocuments.jsx
@@ -1,26 +1,35 @@
 ﻿// Author: Raphael Matto
 // Date: Dec 15, 2023
 // This script updates paths for moved InDesign Documents in an InDesign Book.
-// Tested on MacOs, probably doesn't work on Windows.
+// Tested on MacOs, might work on Windows.
 
 main();
 
 function main() {
-    var newRoot = new Folder(prompt("New folder path", ""));
-
-    if (!newRoot.exists) {
-        alert(newRoot + " does not exist.");
+    try {
+        var book = app.activeBook;
+    } catch(e) {
+        alert(e);
         exit();
     }
 
-    var book = app.activeBook;
+    alert("In the next dialog, select the folder where you've moved the Book's Document files to.")
+
+    var newRoot = Folder.selectDialog("New Documents folder");
+
+    if (!newRoot) {
+        alert("No folder selected, exiting ...");
+        exit();
+    }
 
     var userConfirmation = confirm(
-        "This script will replace any missing Document paths with Document paths that exist at " + 
-        newRoot + " for the book " + book.name + "." + " Do you want to continue?", true, "Please confirm");
+        "This script will replace all missing Document paths with Document paths that exist at " +
+        newRoot + " for the Book " + book.name + "." + " Do you want to continue?", true, "Please confirm");
     if (!userConfirmation) {
         exit();
     }
+
+    var count = 0;
 
     for (var i = 0; i < book.bookContents.length; i++) {
         var doc = book.bookContents[i];
@@ -29,10 +38,14 @@ function main() {
             newPath = new File(newRoot.fsName + "/" + doc.name);
 
             if (newPath.exists) {
-                doc.replace(new File(newRoot.fsName + "/" + doc.name));
+                doc.replace(newPath);
+                count++;
             } else {
                 alert(newPath + " does not exist, skipping ...")
             }
         }
     }
+
+    alert("Paths updated for " + count + " Documents in " + book.name + ".")
+
 }
